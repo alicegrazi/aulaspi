15676f16fd41b1c52f53a2d0140c5dd51af5de4b
Alice Grazielly da Silva Cunha <alice.grazielly@academico.ifrn.edu.br>
2024-10-21 17:42:41 -0300

    Criando o controller de eventos

---
+++ src/main/java/alice/pi/eventos/controlles/EventosController.java
@@ -0,0 +1,14 @@
+package alice.pi.eventos.controlles;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class EventosController {
+	
+	 @RequestMapping("/eventos/form")
+	public String form() {
+		return "formEventos";
+	}
+
+}
