bd1c7ab101e017ebec1b7225e2e787e4b1e6c1bd
Alice Grazielly da Silva Cunha <alice.grazielly@academico.ifrn.edu.br>
2024-11-04 17:57:14 -0300

    iniciando configuração do banco de dados

--- src/main/java/alice/pi/eventos/controlles/EventosController.java
+++ src/main/java/alice/pi/eventos/controlles/EventosController.java
@@ -1,20 +1,17 @@
 package alice.pi.eventos.controlles;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
   
 @Controller
 public class EventosController {
 	
-	@Autowired
-	private EventoRepository er;
+	
 	
 	 @RequestMapping("/eventos/form")
 	public String form() {
 		return "formevento";
 	}
 
-  @PostMapping
+ 
 }
